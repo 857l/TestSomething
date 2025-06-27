7778ce5e89d8bd0e8213903162bde3fd76a4aefb
857l <nik.gusev.2017@bk.ru>
2025-06-27 18:40:08 +0300

    commit

--- app/src/main/java/ru/n857l/testsomething/MainActivity.kt
+++ app/src/main/java/ru/n857l/testsomething/MainActivity.kt
@@ -1,9 +1,14 @@
 package ru.n857l.testsomething
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.core.widget.doAfterTextChanged
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import ru.n857l.testsomething.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding .inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.checkBox.setOnCheckedChangeListener { _, isChecked ->
@@ -23,6 +28,21 @@ class MainActivity : AppCompatActivity() {
 
         binding.editText.doAfterTextChanged {
             binding.checkBox.isEnabled = !it.isNullOrEmpty()
+        }
+
+        binding.loginButton.setOnClickListener {
+            val dialog = BottomSheetDialog(this).apply {
+                setContentView(R.layout.dialog)
+                setCancelable(false)
+
+            }
+
+            val closeButton = dialog.findViewById<ImageButton>(R.id.closeButton)
+            closeButton?.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            dialog.show()
         }
 
     }
