ad0e8e5b2f7e1630206fdfea0d5f728e0bc203a3
857l <nik.gusev.2017@bk.ru>
2025-06-27 06:22:38 +0300

    commit

--- app/src/main/java/ru/n857l/testsomething/MainActivity.kt
+++ app/src/main/java/ru/n857l/testsomething/MainActivity.kt
@@ -3,12 +3,27 @@ package ru.n857l.testsomething
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import ru.n857l.testsomething.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-    }
 
+        binding = ActivityMainBinding .inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.checkBox.setOnCheckedChangeListener { _, isChecked ->
+            binding.loginButton.isEnabled = isChecked
+        }
+
+        binding.editText.doAfterTextChanged {
+            binding.checkBox.isEnabled = !it.isNullOrEmpty()
+        }
+
+    }
 }
