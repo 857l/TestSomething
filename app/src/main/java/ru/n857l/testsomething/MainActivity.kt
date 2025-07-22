974aaec856604de5f291377b037254d0a21a7442
857l <nik.gusev.2017@bk.ru>
2025-07-22 15:07:57 +0300

    deleted imports

--- app/src/main/java/ru/n857l/testsomething/MainActivity.kt
+++ app/src/main/java/ru/n857l/testsomething/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import ru.n857l.testsomething.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
